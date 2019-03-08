0cddfe8afc6d2558a8cfbab77d3d95718620b02f
Raul Tinelli Salles <raul.tinelli@gmail.com>
2019-03-08 16:00:20 -0300

    fim

--- src/components/CompareList/index.js
+++ src/components/CompareList/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Container, Repository } from './styles'
 
 
-const CompareList = ({ repositories }) => (
+const CompareList = ({ repositories, removeRepository, updateRepository }) => (
     <Container>
         { repositories.map(repository => (
 
-            <Repository>
+            <Repository key={ repository.id }>
             <header>
                 <img src={repository.owner.avatar_url} alt={repository.owner.login} />
                 <strong> {repository.name} </strong>
@@ -23,14 +24,43 @@ const CompareList = ({ repositories }) => (
                 {repository.open_issues_count} <small>issues</small>
                 </li>
                 <li>
-                {repository.pushed_at} <small>last commit</small>
+                {repository.lastCommit} <small>last commit</small>
                 </li>
             </ul>
+            <div className="buttons-container">
+                <button type="button" onClick={() => updateRepository(repository.id)}>
+                    <i className="fa fa-retweet" />
+                    Atualizar
+                </button>
+                <button type="button" onClick={() => removeRepository(repository.id)}>
+                    <i className="fa fa-trash" />
+                    Excluir
+                </button>
+            </div>
             </Repository>
 
         )) }
 
     </Container>
 );
+
+CompareList.propTypes = {
+    repositories: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number,
+        name: PropTypes.string,
+        owner: PropTypes.shape({
+          login: PropTypes.string,
+          avatar_url: PropTypes.string,
+        }),
+        stargazers_count: PropTypes.number,
+        forks_count: PropTypes.number,
+        open_issues_count: PropTypes.number,
+        pushed_at: PropTypes.string,
+      }),
+    ).isRequired,
+    removeRepository: PropTypes.func.isRequired,
+    updateRepository: PropTypes.func.isRequired,
+  }
 
 export default CompareList;
